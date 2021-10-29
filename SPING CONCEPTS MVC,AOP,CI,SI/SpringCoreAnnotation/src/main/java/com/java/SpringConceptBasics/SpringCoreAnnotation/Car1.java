091c9c1fbcba836f106508045f2c9c6719974d85
Geethanjali Anbalagan <geethanjali30@gmail.com>
2021-10-29 17:39:58 +0530

    Create Car1.java

---
+++ SPING CONCEPTS MVC,AOP,CI,SI/SpringCoreAnnotation/src/main/java/com/java/SpringConceptBasics/SpringCoreAnnotation/Car1.java
@@ -0,0 +1,17 @@
+package com.java.SpringConceptBasics.SpringCoreAnnotation;
+
+
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class Car1 implements vechile {
+
+	public void drive()
+	
+	{
+		System.out.println("i m not good stressed car block");
+	}
+
+}
