78b84a2597d7d59cfae1ba29f6e91673bda571cc
Geethanjali Anbalagan <geethanjali30@gmail.com>
2021-10-29 17:55:12 +0530

    Create Student.java

---
+++ SPING CONCEPTS MVC,AOP,CI,SI/SpringCoreWithBean/src/main/java/com/java/SpringConceptBasics/SpringCoreWithBean/Student.java
@@ -0,0 +1,17 @@
+package com.java.SpringConceptBasics.SpringCoreWithBean;
+
+public class Student {  
+private String name;  
+  
+public String getName() {  
+    return name;  
+}  
+  
+public void setName(String name) {  
+    this.name = name;  
+}  
+  
+public void displayInfo(){  
+    System.out.println("Hello: "+name);  
+}  
+}  
