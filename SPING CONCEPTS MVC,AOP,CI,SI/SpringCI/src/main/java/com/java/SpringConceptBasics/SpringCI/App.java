b54d2421daacca8071862e84a0c2bddb973b6569
Geethanjali Anbalagan <geethanjali30@gmail.com>
2021-10-29 17:36:48 +0530

    Create App.java

---
+++ SPING CONCEPTS MVC,AOP,CI,SI/SpringCI/src/main/java/com/java/SpringConceptBasics/SpringCI/App.java
@@ -0,0 +1,17 @@
+package com.java.SpringConceptBasics.SpringCI;
+
+import org.springframework.beans.factory.BeanFactory;  
+import org.springframework.beans.factory.xml.XmlBeanFactory;  
+import org.springframework.core.io.*;  
+  
+public class App {  
+    public static void main(String[] args) {  
+          
+        Resource r=new ClassPathResource("applicationContext.xml");  
+        BeanFactory factory=new XmlBeanFactory(r);  
+          
+        Employee s=(Employee)factory.getBean("e");  
+        s.show();  
+          
+    }  
+}  
