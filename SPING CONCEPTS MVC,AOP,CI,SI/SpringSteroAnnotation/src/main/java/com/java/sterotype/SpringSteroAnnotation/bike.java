bb3940fc3d5ee68c103258cc95daff19b12de13e
Geethanjali Anbalagan <geethanjali30@gmail.com>
2021-10-29 17:45:49 +0530

    Create bike.java

---
+++ SPING CONCEPTS MVC,AOP,CI,SI/SpringSteroAnnotation/src/main/java/com/java/sterotype/SpringSteroAnnotation/bike.java
@@ -0,0 +1,13 @@
+package com.java.sterotype.SpringSteroAnnotation;
+import org.springframework.stereotype.Component;
+
+@Component
+public class bike implements vechile {
+	
+	public void rider()
+	{
+		System.out.println("You are riding bike");
+		
+	}
+
+}
