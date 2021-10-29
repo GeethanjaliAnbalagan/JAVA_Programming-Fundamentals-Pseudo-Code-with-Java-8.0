c4fb9c488e61c36ec802997f79900309282e3efc
Geethanjali Anbalagan <geethanjali30@gmail.com>
2021-10-29 17:42:11 +0530

    Create HelloWorld.java

---
+++ SPING CONCEPTS MVC,AOP,CI,SI/springCoreSample/src/main/java/com/java/springCoreSample/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.java.springCoreSample;
+
+public class HelloWorld {
+	   private String message;
+
+	   public void setMessage(String message){
+	      this.message  = message;
+	   }
+	   public void getMessage(){
+	      System.out.println("Your Message : " + message);
+	   }
+	}
