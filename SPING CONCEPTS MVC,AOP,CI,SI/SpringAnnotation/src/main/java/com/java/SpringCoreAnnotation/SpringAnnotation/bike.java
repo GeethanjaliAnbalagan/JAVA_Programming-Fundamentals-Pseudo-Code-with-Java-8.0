3682d026c251684266334ebdf5ee1e64ef5a13b6
Geethanjali Anbalagan <geethanjali30@gmail.com>
2021-10-29 17:33:04 +0530

    Create bike.java

---
+++ SPING CONCEPTS MVC,AOP,CI,SI/SpringAnnotation/src/main/java/com/java/SpringCoreAnnotation/SpringAnnotation/bike.java
@@ -0,0 +1,10 @@
+package com.java.SpringCoreAnnotation.SpringAnnotation;
+
+public class bike implements vechile {
+
+	public void rider()
+	{
+		System.out.println("Currently riding bike");
+		
+	}
+}
