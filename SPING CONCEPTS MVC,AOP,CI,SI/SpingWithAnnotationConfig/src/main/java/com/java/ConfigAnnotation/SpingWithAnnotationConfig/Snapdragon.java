b316339a35c2bb17cbcadbc7de9cb068f7d25d41
Geethanjali Anbalagan <geethanjali30@gmail.com>
2021-10-29 17:31:00 +0530

    Create Snapdragon.java

---
+++ SPING CONCEPTS MVC,AOP,CI,SI/SpingWithAnnotationConfig/src/main/java/com/java/ConfigAnnotation/SpingWithAnnotationConfig/Snapdragon.java
@@ -0,0 +1,13 @@
+package com.java.ConfigAnnotation.SpingWithAnnotationConfig;
+
+
+public class Snapdragon implements MobileProcessor {
+
+	@Override
+	public void Processor() {
+		
+		System.out.println("World best deal CPU ");
+
+	}
+
+}
