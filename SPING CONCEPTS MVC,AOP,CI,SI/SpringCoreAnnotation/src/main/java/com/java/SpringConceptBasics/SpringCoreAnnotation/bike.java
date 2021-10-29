6cb61577b5041e1651e7b8ed4b14aabf81288666
Geethanjali Anbalagan <geethanjali30@gmail.com>
2021-10-29 17:39:40 +0530

    Create bike.java

---
+++ SPING CONCEPTS MVC,AOP,CI,SI/SpringCoreAnnotation/src/main/java/com/java/SpringConceptBasics/SpringCoreAnnotation/bike.java
@@ -0,0 +1,12 @@
+package com.java.SpringConceptBasics.SpringCoreAnnotation;
+
+
+public class bike implements vechile  {
+
+public void drive()
+	
+	{
+		System.out.println("i m not good stressed bike block");
+	}
+
+}
