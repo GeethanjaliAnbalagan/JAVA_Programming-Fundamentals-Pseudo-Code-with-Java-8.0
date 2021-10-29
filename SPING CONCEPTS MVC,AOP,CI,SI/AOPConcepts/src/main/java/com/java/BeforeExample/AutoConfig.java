979d9365db327b484cc0d5e19f6a202e7c0f7873
Geethanjali Anbalagan <geethanjali30@gmail.com>
2021-10-29 17:27:56 +0530

    Create AutoConfig.java

---
+++ SPING CONCEPTS MVC,AOP,CI,SI/AOPConcepts/src/main/java/com/java/BeforeExample/AutoConfig.java
@@ -0,0 +1,10 @@
+package com.java.BeforeExample;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.ComponentScan;
+
+@Configurable
+@ComponentScan("com.java.BeforeExample")
+public class AutoConfig {
+
+}
