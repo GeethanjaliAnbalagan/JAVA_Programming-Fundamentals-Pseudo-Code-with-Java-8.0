7a74921d948c88c6ec463ed7df3b8f2d3e4773c5
Geethanjali Anbalagan <geethanjali30@gmail.com>
2021-10-29 17:45:45 +0530

    Create car.java

---
+++ SPING CONCEPTS MVC,AOP,CI,SI/SpringSteroAnnotation/src/main/java/com/java/sterotype/SpringSteroAnnotation/car.java
@@ -0,0 +1,14 @@
+package com.java.sterotype.SpringSteroAnnotation;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class car implements vechile {
+	
+	public void rider()
+	{
+		System.out.println("You are riding car");
+		
+	}
+
+}
